179045689dcc5e892d5de5d85ce6f5fb93eed372
917549390 <917549390@qq.com>
2020-09-21 16:40:16 +0800

    1111

--- app/Controller/Admin/CouponController.php
+++ app/Controller/Admin/CouponController.php
@@ -125,6 +125,7 @@ class CouponController extends AbstractController
                 $new[$k]['end'] = empty($v['end'])?'Période de validité'.$v['day'].'journée':date('Y-m-d h:i:s',$v['start']).'à'.date('Y-m-d h:i:s',$v['end']);
                 $new[$k]['add_time'] = date('Y-m-d h:i:s',$v['add_time']);
             }
+            $new = array_values($new);
             var_dump(objectToArray($new),__LINE__);
             return success(objectToArray($new));
         }else{
